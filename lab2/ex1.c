6505cfa7b0a2eecbf2b4380c01dc8a71c434f52d
Felipe Veeck <142080271+Veeck1@users.noreply.github.com>
2026-03-11 01:43:39 +0000

    implement ex1 functions lab2

--- lab2/ex1.c
+++ lab2/ex1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 void dump (void *p, int n) {
   unsigned char *p1 = p;
@@ -8,8 +9,56 @@ void dump (void *p, int n) {
   }
 }
 
+int string2num(char *s, int base) {
+    int a = 0;
+    for (; *s; s++) {
+        a = a * base + (*s - '0');
+    }
+    return a;
+}
+
 int main (void) {
+
+  printf("Int\n");
   int i = 10000;
   dump(&i, sizeof(i));
+
+  printf("\n");
+  printf("Long int\n");
+  long d = 10000;
+  dump(&d, sizeof(d));
+
+  printf("\n");
+  printf("Short int\n");
+  short s = 10000;
+  dump(&s, sizeof(s));
+
+  printf("\n");
+  printf("Char\n");
+  char p = 'a';
+  char g = 97;
+  dump(&p, sizeof(p));
+  printf("%c\n", g);
+
+  printf("\n");
+  printf("Vetor de Char 1\n");
+  char t[] = "7509";
+  dump(t, sizeof(t));
+
+  printf("\n");
+  printf("Vetor de Char 2\n");
+  char v[] = {'A',' ','\n','$'};
+  dump(v, sizeof(v));
+
+  printf("\n");
+  printf("Questao 2\n");
+  
+  printf("==> %d\n", string2num("1234", 10));
+  printf("==> %d\n", string2num("1234", 10) + 1);
+  printf("==> %d\n", string2num("1234", 10) + string2num("1", 10));
+
+  printf("%d\n", string2num("777", 8));
+  printf("%d\n", string2num("777", 10));
+  
   return 0;
 }
